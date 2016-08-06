cb10a447ba37e85275970784395b6185d52b7001
John Clarke <john.clarke@gmail.com>
2016-08-06 05:54:37 +0100

    Added instruction comment to status.php

--- status.php
+++ status.php
@@ -1,8 +1,14 @@
 <?php
 
+ /*
+  * Simple file to spit out http response codes
+	* to easily to test traffic light widgets
+	* eg. status.php?404 = 404 Error
+	*/
+
 	$code = (int) $_SERVER['QUERY_STRING'];
-	
+
 	http_response_code($code);
-	
+
 
 ?>
